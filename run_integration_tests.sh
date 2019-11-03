a43f275e820db79b913e13d931087fe73a0faf5a
Marek Milkovič <marek.milkovic@avast.com>
2019-11-03 21:18:18 +0100

    Fixed installation of python dependencies and installation in travis

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install -r tests/integration/requirements.txt
+pip3 install -r tests/integration/requirements.txt
 SERVER_PATH=$(pwd)/build/tests/integration/server/server python3 -m pytest -vv tests/integration/test.py
